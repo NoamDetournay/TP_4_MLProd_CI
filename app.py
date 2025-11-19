400699051f7884611c8e74a73e3ff6d69d5982ed
NoamDetournay <noam.detournay@efrei.net>
2025-11-19 11:53:01 +0100

    change to dev

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ items = []
 
 @app.route('/')
 def index():
-    return render_template('index.html', items=items)
+ return render_template('index.html', items=items)
 
 @app.route('/add', methods=['POST'])
 def add_item():
